f2f6f144e8aed245a3e9fb4ba48a9950d7fef8d1
SofiaRubiano10 <10sofiarubiano@gmail.com>
2023-07-10 23:23:52 +0200

    Creación del context

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-// import PersonalData from "./components/PersonalData";
-// import Hobbys from "./components/Hobbys";
-// import Education from "./components/Education";
-// import Happiness from "./components/Happiness";
+import { useState } from "react";
+import PersonalData from "./components/PersonalData";
+import Hobbys from "./components/Hobbys";
+import Education from "./components/Education";
+import Happiness from "./components/Happiness";
 import Music from "./components/Music";
+import FormContext from "./context/FormContext";
 
 function App() {
+  const [step, setStep] = useState("music");
   return (
-    <>
+    <FormContext.Provider value={{ step, setStep }}>
       <div className="container">
-        {/* <PersonalData /> */}
-        {/* <Hobbys /> */}
-        {/* <Education /> */}
-        {/* <Happiness /> */}
-        <Music />
+        {step === "personaldata" && <PersonalData />}
+        {step === "hobbys" && <Hobbys />}
+        {step === "education" && <Education />}
+        {step === "happiness" && <Happiness />}
+        {step === "music" && <Music />}
       </div>
-    </>
+    </FormContext.Provider>
   );
 }
 
